6d9aaf83c28b4cf56cfa94b3e072dbb8455671db
minhtu <ptlinh.24174600106@sv.uneti.edu.vn>
2024-10-24 09:59:39 +0700

    bai_1_chuong_6

---
+++ bai_1.py
@@ -0,0 +1,14 @@
+import math
+
+r=float(input("nhap ban kinh: ")) 
+h=float(input("nhap chieu cao: "))
+pi = 3.14
+dtxq = 2*pi*r*h
+dttp = dtxq + 2*pi*r**2
+the_tich = pi*r**2*h
+if r > 0 and h > 0:
+   print(f"Dien tich xung quanh: {dtxq:.2f}")
+   print(f"Dien tich toan phan: {dttp:.2f}")
+   print(f"The tich: {the_tich:.2f}")
+else:
+   print("Loi gia tri vui long nhap lai") 
