7038edf209ff11b75a3613a377944482ebff87fa
minhtu <ptlinh.24174600106@sv.uneti.edu.vn>
2024-10-24 16:01:49 +0700

    bai_8_chuong567

--- bai_8.py
+++ bai_8.py
@@ -2,10 +2,10 @@ import math
 
 x = float(input("nhap gia tri: ")) 
 if x > 0 and x != 1 :
-    print("gia tri thoa man")
+    logx4 = math.log(x,4)
+    log2x = math.log(2,x)
+    f_x = logx4 + log2x
+    print(f"Gia tri can tim f(x) = {f_x:.2f}")
 else :
-     print(" Loi gia tri ") 
-logx4 = math.log(x,4)
-log2x = math.log(2,x)
-f_x = logx4 + log2x
-print(f"Gia tri can tim x:{f_x:.2f}")    
+      print("loi gia tri")
+   
